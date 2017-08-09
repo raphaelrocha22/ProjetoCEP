10dbb3379923504c2a8d2f170fc40262144b8fb9
raphaelrocha22 <raphael.2205@gmail.com>
2017-08-08 23:11:50 -0300

    cadastro amostra no banco e validacao dos campos

--- ProjetoCEP/Projeto.DAL/Persistencias/CepDAL.cs
+++ ProjetoCEP/Projeto.DAL/Persistencias/CepDAL.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using Projeto.DAL.Conexoes;
+using Projeto.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projeto.DAL.Persistencias
 {
-    public class CepDAL
+    public class CepDAL:Conexao
     {
+        public void CadastrarAmostras(Lote l)
+        {
+            AbrirConexao();
 
+            string query = "Insert into Amostras (Lote,DataCriacao,IdOperadorCriacao,DataAnalise,IdOperadorAnalise,TotalLentes,QtdNaoConforme,Percentual) " +
+                "values (@Lote,@DataCriacao,@IdOperadorCriacao,@DataAnalise,@IdOperadorAnalise,@TotalLentes,@QtdNaoConforme,@Percentual)";
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Lote", l.NumeroLote);
+            cmd.Parameters.AddWithValue("@DataCriacao", l.DataCriacao);
+            cmd.Parameters.AddWithValue("@IdOperadorCriacao", l.IdOperadorCriacao);
+            cmd.Parameters.AddWithValue("@DataAnalise", l.DataAnalise);
+            cmd.Parameters.AddWithValue("@IdOperadorAnalise", l.IdOperadorAnalise);
+            cmd.Parameters.AddWithValue("@TotalLentes", l.TotalLentes);
+            cmd.Parameters.AddWithValue("@QtdNaoConforme", l.QtdNaoConforme);
+            cmd.Parameters.AddWithValue("@Percentual", l.Percentual);
+            cmd.ExecuteNonQuery();
+
+            FecharConexao();
+        }
+
+        public List<Lote> ObterAmostras()
+        {
+            AbrirConexao();
+
+            string query = "select * from Amostras";
+
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+
+            var lista = new List<Lote>();
+
+            while (dr.Read())
+            {
+                var l = new Lote();
+                l.IdLote = (int)dr["IdLote"];
+                l.NumeroLote = (int)dr["Lote"];
+                l.DataCriacao = (DateTime)dr["DataCriacao"];
+                l.IdOperadorCriacao = (int)dr["IdOperadorCriacao"];
+                l.DataAnalise = (DateTime)dr["DataAnalise"];
+                l.IdOperadorAnalise = (int)dr["IdOperadorAnalise"];
+                l.TotalLentes = (int)dr["TotalLentes"];
+                l.QtdNaoConforme = (int)dr["QtdNaoConforme"];
+                l.Percentual = (decimal)dr["Percentual"];
+
+                lista.Add(l);
+            }
+            FecharConexao();
+            return lista;
+        }
     }
 }
--- ProjetoCEP/Projeto.WEB/Areas/AreaRestrita/Controllers/CEPController.cs
+++ ProjetoCEP/Projeto.WEB/Areas/AreaRestrita/Controllers/CEPController.cs
@@ -1,4 +1,5 @@
-﻿using Projeto.Entidades;
+﻿using Projeto.DAL.Persistencias;
+using Projeto.Entidades;
 using Projeto.Util;
 using Projeto.WEB.Areas.AreaRestrita.Models.CEP;
 using System;
@@ -6,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.UI.WebControls;
 
 namespace Projeto.WEB.Areas.AreaRestrita.Controllers
 {
@@ -29,22 +30,44 @@ namespace Projeto.WEB.Areas.AreaRestrita.Controllers
         {
             if (ModelState.IsValid)
             {
-                try
+                if (model.DataAnalise >= model.DataCriacao && model.TotaLentes >= model.QtdNaoConforme)
                 {
-                    var l = new Lote();
-                    l.NumeroLote = model.Lote;
-                    l.DataCriacao = model.DataCriacao;
-                    l.IdOperadorCriacao = model.IdOperadorCriacao;
-                    l.DataAnalise = model.DataAnalise;
-                    l.IdOperadorAnalise = model.IdOperadorAnalise;
-                    l.TotalLentes = model.TotaLentes;
-                    l.QtdNaoConforme = model.QtdNaoConforme;
-                    l.Percentual = (model.QtdNaoConforme / model.TotaLentes);
+                    try
+                    {
+                        var l = new Lote();
+                        l.NumeroLote = model.Lote;
+                        l.DataCriacao = model.DataCriacao;
+                        l.IdOperadorCriacao = model.IdOperadorCriacao;
+                        l.DataAnalise = model.DataAnalise;
+                        l.IdOperadorAnalise = model.IdOperadorAnalise;
+                        l.TotalLentes = model.TotaLentes;
+                        l.QtdNaoConforme = model.QtdNaoConforme;
+                        l.Percentual = (model.QtdNaoConforme / model.TotaLentes);
+
+                        var d = new CepDAL();
+                        d.CadastrarAmostras(l);
+
+                        ModelState.Clear();
+                        TempData["MensagemCadastro"] = $"Lote {model.Lote}, incluído com sucesso.";
+                    }
+                    catch (Exception e)
+                    {
+                        if (e.HResult == -2146232060)
+                        {
+                            TempData["MensagemErro"] = "Um lote com essa numeração já foi inserido no sistema";
+                        }
+                        else
+                        {
+                            TempData["MensagemErro"] = "Erro não esperado. Tente novamente, se o erro persistir entre em contato com o administrador do sistema";
+                            Logger.LogErro(HttpContext.Server.MapPath("/bin/Logs/"), e);
+                        }
+                    }
                 }
-                catch (Exception e)
+                else
                 {
-                    ViewBag.Mensagem = "Erro não esperado. Tente novamente, se o erro persistir entre em contato com o administrador do sistema";
-                    Logger.LogErro(HttpContext.Server.MapPath("/bin/Logs/"), e);
+                    TempData["MensagemErro"] = "Por favor, verifique se a data de análise não está inferior a data de criação " +
+                                                "ou se a quantidade não conforme não está maior que o total de lentes";
+
                 }
             }
 
